6a8bc1fa5b3e514ff1322fec895b8fcdb74b8931
Craig Group <rcg6p@virginia.edu>
2022-11-29 14:08:06 -0500

    put a more reasonable N in myslurm_array

--- myslurm_array.sh
+++ myslurm_array.sh
@@ -10,7 +10,7 @@
 #SBATCH --error=MC_pi_%A_%a.error
 
 
-python ./MC_pi.py 1000000000 ${SLURM_ARRAY_TASK_ID}
+python ./MC_pi.py 100000000 ${SLURM_ARRAY_TASK_ID}
 
 #This is a comment explaining how to run it...  
 #sbatch --array=101-102 myslurm_array.sh
